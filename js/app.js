6f26b7eaf28cf86f59d9b0a28b44db282f9b8537
Jay-vaghani <jayvaghani10@gmail>
2023-02-24 22:43:04 +0530

    ok

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let contactForm = document.getElementsByClassName("contact-form")[0];
+
+const sjc = () => {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let number = document.getElementById("number").value;
+  let textarea = document.getElementById("textarea").value;
+
+  Email.send({
+    SecureToken: "6f93c6fd-285e-4d9f-9f0f-4e6c19e6fa44",
+    To: "vaghanijay20@gmail.com",
+    From: "vaghanijay20@gmail.com",
+    Subject: "This is the subject",
+    Body: "And this is the body",
+  }).then((message) => alert(message));
+};
